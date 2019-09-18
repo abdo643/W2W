4f7322606f7ad351d2881bb53bba35ca8881eefd
abdo643 <abou.shehata643@yahoo.de>
2019-09-18 12:34:51 +0200

    neu

--- w2w/src/app/app.module.ts
+++ w2w/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BooksComponent } from './books/books.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BooksComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
