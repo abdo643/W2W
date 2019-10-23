b82e80cd002fb57372a551c42c380e93e6646b61
abdo643 <abou.shehata643@yahoo.de>
2019-10-23 11:22:28 +0200

    falsches programm

--- Protokollprogramm/my-app/src/app/mock-stundents.ts
+++
@@ -1,9 +0,0 @@
-import {Student} from './classes/student';
-
-export const STUDENTS : Student[] = [
-    {id: 11,name: 'Bauer'},
-    {id: 12,name: 'Dado'},
-    {id: 13,name: 'Mado'},
-    {id: 14,name: 'Karla'},
-    {id: 14,name: 'Samuel'}
-];
