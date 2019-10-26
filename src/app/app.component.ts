7ed72c0cf1006529ce59cda1ff474246a9822604
abdo643 <abou.shehata643@yahoo.de>
2019-10-26 18:27:16 +0200

    probe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,19 @@
 import { Component, OnInit, Pipe,PipeTransform } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { MessageService } from './message.service';
 import { catchError, map, tap } from 'rxjs/operators';
 import { DomSanitizer } from '@angular/platform-browser'
-import { MessageService } from './message.service';
-
-export interface Items{
-  netflixid: string;
-  title: string;
-  image: string;
-  synopsis: string;
-  rating: string;
-  type: string;
-  released: string;
-  runtime: string;
-  largeimage: string;
-  unogsdate: string;
-  imdbid: string;
-  download: string;
-}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-/* ={
-  netflixid: "",
-  title: "",
-  image: "",
-  synopsis: "",
-  rating: "",
-  type: "",
-  released: "",
-  runtime: "",
-  largeimage: "",
-  unogsdate: "",
-  imdbid: "",
-  download: "", };
-*/
 export class AppComponent 
 {
   title = 'Welcome to W2W';
   public data:any;
-  public dataValue: Items[]=[];
+  public dataValue:any;
 
   private isDataAvailable:boolean=false;
 
@@ -52,12 +23,7 @@ export class AppComponent
     let headers = new HttpHeaders().set('x-rapidapi-host', 'unogs-unogs-v1.p.rapidapi.com');
     headers = headers.set('x-rapidapi-key', '2e75f12489msh4881df0eea4530ap1d9974jsnb1c9075c37c4');
     headers.append('Content-Type', 'application/json');
-    this.http.get<Items[]>("https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Anew7%3AUS&p=1&t=ns&st=adv",{ headers: headers}).
+    this.http.get<any>("https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Anew7%3ADE&p=1&t=ns&st=adv",{ headers: headers}).
     subscribe(data => {this.dataValue = data;console.log(this.dataValue);});      
-    /*this.http.get('https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Anew7%3AUS&p=1&t=ns&st=adv',{ headers: headers}),
-    function Data(data:any)
-    {
-      console.log(data);   
-    }*/
   }    
 }
