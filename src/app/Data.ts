655db6054c8cfd9faf84e99c1a37eafdcdba9a0f
abdo643 <abou.shehata643@yahoo.de>
2019-11-20 10:51:29 +0100

    update

---
+++ src/app/Data.ts
@@ -0,0 +1,19 @@
+export class obj{
+    count: string;
+    items: Item[];
+    proto: any;
+  }
+  class Item{
+  netflixid: string;
+  title: string;
+  image: string;
+  synopsis: string;
+  rating: string;
+  type: string;
+  released: string;
+  runtime: string;
+  largeimage: string;
+  unogsdate: string;
+  imdbid: string;
+  download: string;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,37 +3,22 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { MessageService } from './message.service';
 import { catchError, map, tap } from 'rxjs/operators';
 import { DomSanitizer } from '@angular/platform-browser'
-
-export interface IData{
-  COUNT: string;
-  ITEMS: Array<Item>;
-}
-interface Item{
-  netflixid: string;
-  title: string;
-  image: string;
-  synopsis: string;
-  rating: string;
-  type: string;
-  released: string;
-  runtime: string;
-  largeimage: string;
-  unogsdate: string;
-  imdbid: string;
-  download: string;
-}
+import { throwError, from } from 'rxjs';
+import {obj} from './Data'
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.sass']
 })
 
 export class AppComponent 
 {
   title = 'Welcome to W2W';
-  //public data:Obj;
-  public dataValue: IData[]=[];
+  data:any;
+  public dataValue: obj[];
+  public expire: obj[];
 
   private isDataAvailable:boolean=false;
 
@@ -43,7 +28,16 @@ export class AppComponent
     let headers = new HttpHeaders().set('x-rapidapi-host', 'unogs-unogs-v1.p.rapidapi.com');
     headers = headers.set('x-rapidapi-key', '2e75f12489msh4881df0eea4530ap1d9974jsnb1c9075c37c4');
     headers.append('Content-Type', 'application/json');
-    this.http.get<IData[]>("https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Anew7%3ADE&p=1&t=ns&st=adv",{ headers: headers}).
-    subscribe(data => {this.dataValue = data;console.log(this.dataValue);});      
-  }    
+    this.http.get<any>("https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Anew7%3ADE&p=1&t=ns&st=adv",{ headers: headers}).
+    subscribe(data => {this.dataValue = data;console.log(this.dataValue);});
+    /*this.http.get("https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Anew7%3ADE&p=1&t=ns&st=adv",{ headers: headers}).
+    pipe(map((data: any) => data.result), catchError(error => {
+    return throwError('Its a Trap!')})).subscribe(data => {this.dataValue = data;console.log(this.dataValue)});*/      
+    this.http.get<any>("https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Aexp%3ADE&t=ns&st=adv&p=1",{ headers: headers}).
+    subscribe(data => {this.expire = data;console.log(this.expire);});
+    
+  }
 }
+
+
+
